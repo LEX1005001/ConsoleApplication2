095c13cd71133f4913f2dd7ddeb960c2c43c7338
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-01-06 19:28:16 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,13 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+int main()
+{
+	int number, d1, d2, d3, d4;
 
-int main() {
-	int km = 0;
-	int hour = 0;
+	cout << "4-number: " "\n";
+	cin >> number;//1234
 
-	cout << "distance in km: ";
-	cin >> km;
-	cout << "time in hours: ";
-	cin >> hour;
-	cout << "You need to go at speed--> " << km / hour << " km/h";
+	if (number < 1000 || number > 9999) {
+		cout << "wrong nummber :/";
+	}
+	else {
+		cout << "correct number";
+	}//1234
+	d4 = number % 10;//4
+	number = number / 10;//123
+	d3 = number % 10;//3
+	number = number / 10;//12
+	d2 = number % 10;//2
+	number = number / 10;//1
+	d1 = number % 10;//1
+	cout << d2 << d1 << d4 << d3;
+	
 }
