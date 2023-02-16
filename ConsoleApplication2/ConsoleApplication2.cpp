c2b3ee2e40f6be63b29d25f4946e64190b961b5e
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-02-16 17:46:36 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,53 +1,62 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
-int main()
-{
-	int number, d1, d2, d3, d4, d5, d6, sum1, sum2;
+int main() {  
+    setlocale(LC_ALL, "Russian");
+    /*int a;
+    int sum = 0;
+    cout << "a:";
+    cin >> a;
 
-	cout << " 6-digit number:\n";
-	cin >> number;//123456
-	if (number / 100000 < 1)
-	{
-		cout << "Your number includes less than "
-			"6 digits!\n";
-	}
-	else if (number / 100000 > 9)
-	{
-		cout << "Your number includes more than "
-			"6 digits!\n";
-	}
-	else
-	{
-		cout << "Your number includes exactly "
-			"6 digits!\n";
-	}
+    while (a < 500) {
+        sum += a;
+        a++;
+    }
+    cout << sum << endl;
+    cout << "Задание 1" << endl;*/
 
-	
-	d6=number % 10; //6
-	number = number / 10;//12345
 
-	d5 = number % 10;//5
-	number = number / 10;//1234
+    /*int x, y;
 
-	d4 = number % 10;//4
-	number = number / 10;//123
+    cout << "x:";
+    cin >> x;
+    cout << "y:";
+    cin >> y;
+    cout << pow(x, y) << endl;
+    cout << "Задание 2" << endl;*/
 
-	d3 = number % 10;//3
-	number = number / 10;//12
 
-	d2 = number % 10;//2
-	number = number / 10;//1
+    /*float sred_arifm;
+    sred_arifm = 0;
+    for (int i = 1; i <= 1000; i++)
+    {
+        sred_arifm += i;
+    }
+    sred_arifm /= 1000;
+    cout << "среднее арифметическое:" << sred_arifm << endl;
+    cout << "Задание 3" << endl;*/
 
-	d1 = number % 10;//1
-	
-	sum1 = d1 + d2 + d3;
-	sum2 = d4 + d5 + d6;
+    /*int b;
+    int pr = 1;
 
-	if (sum1 == sum2) {
-		cout << "correct number, you are lucky!! :)";
-	}
-	else {
-		cout << "you didn't guess the number";
-	}
+    cout << "b:";
+    cin >> b;
+
+    for(int i=b;i<20;i++){
+        pr = pr * i;
+    }
+    cout << "Произведение=" << pr << endl;
+    cout << "Задание 4" << endl;*/
+
+    int k;
+
+    cout << "k:";
+    cin >> k;
+
+    for (int i = 2; i < 10; i++) {
+        cout << k << "*" << i << "=" << k * i << endl;
+    }
+    cout << "Задание 5" << endl
+    
 
 }
