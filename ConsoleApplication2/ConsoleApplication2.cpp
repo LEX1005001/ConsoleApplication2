688caa691083272d095a45484527cebdb9ec0b56
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-02-16 16:50:16 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,13 +1,51 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+int main() {  
+    setlocale(LC_ALL, "Russian");
+    /*float R1;
+    float R2;
+    float R3;
+    float R0;
+    cout << "R1:";
+    cin >> R1;
+    cout << "R2:";
+    cin >> R2;
+    cout << "R3:";
+    cin >> R3;
 
-int main() {
-	int km = 0;
-	int hour = 0;
+    R0 = 1 / R1 + 1 / R2 + 1 / R3;
 
-	cout << "distance in km: ";
-	cin >> km;
-	cout << "time in hours: ";
-	cin >> hour;
-	cout << "You need to go at speed--> " << km / hour << " km/h";
+    cout << "1/R0 = " << 1 / R0 << "\n";
+    cout<<"Задание 1\n";*/
+
+    /*float L;
+    float pi = 3.14;
+    float R;
+    float S;
+
+    cout << "L: ";
+    cin >> L;
+
+    R = L / (2 * pi);
+    S = pi * (R * R);
+
+    cout << "S:" << S << "\n";
+    cout << "Задание 2\n";*/
+
+    float S1;
+    float v;
+    float t;
+    float a;
+
+    cout << "v:";
+    cin >> v;
+    cout << "t:";
+    cin >> t;
+    cout << "a:";
+    cin >> a;
+
+    S1 = v * t + (a * (t * t)) / 2;
+    cout << "S:" << S1 << "\n";
+    cout << "Задание 3\n";
+
 }
